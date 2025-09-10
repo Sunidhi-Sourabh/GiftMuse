34b9f06712ed4c6b9df39f9b98cb352b6b5fda2a
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-09-10 21:39:02 +0530

    GiftMuseForm.js

--- src/frontend/GiftMuseForm.js
+++ src/frontend/GiftMuseForm.js
@@ -1,1 +1,17 @@
-
+fetch("http://<giftmuse-agent-service>/generate-bundle", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    occasion: "...",
+    recipient_gender: "...",
+    relation: "...",
+    budget: "...",
+    rating: "...",
+    likes: "...",
+    dislikes: "..."
+  })
+})
+.then(res => res.json())
+.then(data => {
+  // Redirect to /giftmuse-result and show the bundle
+})
