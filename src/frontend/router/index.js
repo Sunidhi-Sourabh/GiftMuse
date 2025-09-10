9104f72b0cfc2b6ea0d47e4d724a1879c007ecb9
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-09-10 21:54:30 +0530

    index.js

--- src/frontend/router/index.js
+++ src/frontend/router/index.js
@@ -1,1 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  // Add your new route here 👇
+  {
+    path: '/giftmuse-result',
+    name: 'GiftMuseResult',
+    component: () => import('../pages/GiftMuseResult.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
