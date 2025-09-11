94cc5d944f1dba276213ad005b301a605058412d
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-09-11 12:58:03 +0530

    GiftMuseForm.vue

--- src/frontend/GiftMuseForm.vue
+++ src/frontend/GiftMuseForm.vue
@@ -1,17 +1,9 @@
-fetch("http://<giftmuse-agent-service>/generate-bundle", {
+fetch("http://localhost:8080/generate-bundle", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    occasion: "...",
-    recipient_gender: "...",
-    relation: "...",
-    budget: "...",
-    rating: "...",
-    likes: "...",
-    dislikes: "..."
-  })
+  body: JSON.stringify(formData)
 })
 .then(res => res.json())
 .then(data => {
-  // Redirect to /giftmuse-result and show the bundle
+  // Redirect to /giftmuse-result and pass bundle data
 })
