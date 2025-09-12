0dd28056b2c787a07a90c10c9da7f35f41673810
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-09-12 14:27:20 +0530

    GiftMuse Form.vue

---
+++ src/GiftMuse Agent/GiftMuse Form.vue
@@ -0,0 +1,9 @@
+fetch("http://localhost:8080/generate-bundle", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(formData)
+})
+.then(res => res.json())
+.then(data => {
+  // Redirect to /giftmuse-result and pass bundle data
+})
